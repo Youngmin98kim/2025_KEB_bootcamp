a49527bcd2222a2e837eab55cca5380ad1cfecbc
Youngmin98kim <sodayoung73@gmail.com>
2025-02-12 17:32:33 +0900

    v1.2 error day03.py

--- day 03.py
+++ day 03.py
@@ -1,28 +1,33 @@
+#assignment
+#메뉴 삭제 추가에 대응되는 코드를 추가 : 반복문, len함수, input 에서 문자열 결합
 import random
 drinks = ['위스키','와인','소주','고량주']
 drinks_food = ['초콜릿','치즈','삼겹살','양꼬치']
 
 drinks.append('사케')
 drinks.append('위스키')
+drinks.append('데킬라')
 
 drinks_food.append('광어회')
 drinks_food.append('낙곱새')
+drinks_food.append('소금')
 
+menu_list = '다음 술 중에 고르세요.\n' 
+for i in range(len(drinks)):
+    menu_list = menu_list + f'{i+1}){drinks[i]} '
+#f'다음 술중에 고르세요.\n 1){drinks[0]}  2){drinks[1]}  3){drinks[2]}  4){drinks[3]}  5){drinks[4]}  6)랜덤 추천  7) 종료 : '
+menu_list = menu_list + f'{len(drinks)+1}) 랜덤 추천 {len(drinks)+2}) 종료 : '
 while True:
-    menu = input(f'다음 술중에 고르세요.\n 1){drinks[0]}  2){drinks[1]}  3){drinks[2]}  4){drinks[3]}  5){drinks[4]}  6)랜덤 추천  7) 종료 : ')
-    if menu == '1':
-        print(f'{drinks[0]}에 어울리는 안주는 {drinks_food[0]} 입니다')
-    elif menu == '2':
-        print(f'{drinks[1]}에 어울리는 안주는 {drinks_food[1]} 입니다')
-    elif menu == '3':
-        print(f'{drinks[2]}에 어울리는 안주는 {drinks_food[2]} 입니다')
-    elif menu == '4':
-        print(f'{drinks[3]}에 어울리는 안주는 {drinks_food[3]} 입니다')
-    elif menu == '5':
-        print(f'{drinks[4]}에 어울리는 안주는 {drinks_food[4]} 입니다')
-    elif menu == '6':
-        random_drink = random.choice(drinks)
-        print(f'{random_drink}에 어울리는 안주는 {drinks_food[drinks.index(random_drink)]} 입니다')
-    elif menu == '7':
-        print(f'다음에 또 오세요')
-        break
+    menu = input(menu_list)
+    if menu == i+1 :
+        print(f'{drinks[i]}에 어울리는 안주는 {drinks_food[i]} 입니다')
+
+    elif menu == len(drinks)+1 :
+        random = random.randint(0,len(drinks)-1)
+        print(f'{drinks[random]}에 어울리는 안주는 {drinks_food[random]}입니다.')
+        #random_drink = random.choice(drinks)
+        #print(f'{random_drink}에 어울리는 안주는 {drinks_food[drinks.index(random_drink)]} 입니다')
+
+    elif menu == len(drinks)+2 :
+       print(f'다음에 또 오세요')
+       break
