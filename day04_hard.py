eaa1ba071b43f9fd8b92f7ab9f1a5c1f1a370c7c
Youngmin98kim <sodayoung73@gmail.com>
2025-02-13 15:27:40 +0900

    v1.1 day04 decorator.py

--- day04_hard.py
+++ day04_hard.py
@@ -10,7 +10,7 @@ def time_decorator(func):
         return r
     return wrapper
 
-@time_decorator
+#@time_decorator
 def factorial_repitition(n) -> int:
     result = 1
     for i in range(2,n+1):
@@ -18,7 +18,59 @@ def factorial_repitition(n) -> int:
     return result
 
 number = int(input())
-s = time.time()
+ft = time_decorator(factorial_repitition)
+print(f"{number}! = {ft(number)}")
+number = int(input())
 print(f"{number}! = {factorial_repitition(number)}")
-e = time.time()
-print(e-s)
+# decorator
+def description(f):  # closure
+    def inner(*args):
+        print(f.__name__)
+        print(f.__doc__)
+        r = f(*args) #함수 실행 가변매개변수를 인자로 받는다
+        return r
+
+    return inner
+
+
+def squares(n):
+    """
+    제곱 함수
+    """
+    return n * n
+
+@description
+def power(b, e):
+    """
+    거듭제곱 함수
+    """
+    result = 1
+    for _ in range(e):
+        result = result * b
+    return result
+
+print(squares(7))
+f1 = description(squares)
+print(f1(9))
+print(power(2, 10))
+
+
+# f2 = description(power)
+# print(f2(2, 10))
+
+# print(squares(7))
+# print(squares.__doc__)
+
+# def my_range(first=0, last=5, step=1):
+#     number = first
+#     while number < last:
+#         yield number
+#         number += step
+#
+# r = my_range()
+# print(r, type(r))
+#
+# for x in r:
+#     print(x)
+# for x in r:
+#     print(x)
