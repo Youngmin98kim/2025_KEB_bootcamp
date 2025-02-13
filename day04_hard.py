eb1bc2f016dad2e5173c011c58ee4be506f50616
Youngmin98kim <sodayoung73@gmail.com>
2025-02-13 14:28:13 +0900

    v1.0 day04 decorator.py

--- day04_hard.py
+++ day04_hard.py
@@ -1,6 +1,16 @@
-#SOLID
+#SOLID  open closed principle 개방 폐쇄 원칙 (확장에는 열려있고 수정에는 닫혀있음)
 import time
 
+def time_decorator(func):
+    def wrapper(*args):
+        s = time.time()
+        r = func(*args)
+        e = time.time()
+        print(f'실행 시간 : {e-s}sec')
+        return r
+    return wrapper
+
+@time_decorator
 def factorial_repitition(n) -> int:
     result = 1
     for i in range(2,n+1):
