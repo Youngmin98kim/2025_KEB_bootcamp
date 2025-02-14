1d5e5e0ec229acedec57ae69923f2e2dbf9c1fdc
Youngmin98kim <sodayoung73@gmail.com>
2025-02-14 10:52:31 +0900

    v1.1 fibonacci day05.py

---
+++ day05_2.py
@@ -0,0 +1,72 @@
+def factorial_repetition(n) -> int:
+    '''
+    반복문을 이용한 팩토리얼 함수
+    :param n: 정수, int
+    :return: 팩토리얼 값, int
+    '''
+    result = 1
+    for i in range(2, n+1):
+        result = result * i
+    return result
+
+#반복문 재귀함수 f(n) = n * f(n-1)
+def factorial_recursion(n):
+    '''
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: 정수, int
+    :return: function, int
+    '''
+    if n == 1:
+        return n
+    else:
+        return n * factorial_recursion(n-1)
+
+def pibo_recursion(n) -> int:
+    """
+    재귀함수를 이용한 피보나치 수열
+    :param n: 정수, int
+    :return: function, int
+    """
+    if n==1:
+        return 1
+    else :
+        return pibo_recursion(n-1) + pibo_recursion(n-2)
+
+def pibo_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a+b
+
+def pibo_pf(n) -> int :
+    """
+    피보나치 수 계산함수(재귀함수)
+    :param n:
+    :return: 피보나치 계산 결과 값
+    """
+    if n <= 0 :
+        return 0
+    elif n == 1:
+        return 1
+    else :
+        return pibo_pf(n-2)+pibo_pf(n-1)
+print(pibo_pf(int(input()))) #return이 계속해서 반복되기 때문에 입력 숫자가 커지면 시간도 오래 걸린다.
+
+
+number = int(input("number : "))
+print(factorial_repetition(number))
+print(factorial_recursion(number))
+print(globals())
+
+def fibo_loop(n) -> int : #시간 엄청 오래걸림.
+    """
+    반복문을 이용한 피보나치 수 계산 함수
+    :param n: int
+    :return: 피보나치 계산 결과 값
+    """
+    n_list = [0,1]
+    for i in range(n+1):
+        n_list.append(n_list[i] + n_list[i+1])
+    return n_list[n]
+n=int(input())
+print(fibo_loop(n))
