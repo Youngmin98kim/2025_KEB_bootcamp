6b39ab12e88d270e2463d6a62745b897733a2dba
Youngmin98kim <sodayoung73@gmail.com>
2025-02-14 17:23:12 +0900

    v1.0module day05.py

--- day05.py
+++ day05.py
@@ -1,19 +1,8 @@
-y=3
-x=y*9
-z=list(range(x))
-print(z)
-print(tuple(map(str,z)))
+import ymmath as ym # 줄여쓰기
 
-def my_range(first=0,last=5, step=1): #default 매개변수
-    number = first #number라는 지역변수에 시작값을 부여
-    while number < last : #number가 last보다 작을 때까지
-        yield number #generator(iterable 반복가능 객체)로 함수를 만들어줌. return이면 코드가 끝남. number return후 그 뒤의 함수를 실행
-        #number += step #yield여서 이게 실행됨.
-        number = number + step
-r = my_range()
-print(r,type(r)) #r이 my range 함수의 return을 받음 , r type은 generator
+print(ym.ym_fibo_loop(3))
+#print(ymmath.my_abs(-6)) as 안 쓴 경우
 
-for x in r:
-    print(x) #yield가 보낸 값이 r에 담겨서 출력됨.
-for x in r:
-    print(x) # 두 번째는 출력되지 않음. 비워짐.
+if __name__ == "__main__" : # day 5에서는 ymmath가 main이 아니어서 실행이 안됨.
+    print(ym.ym_fibo_loop(3))
+    #print(ymmath.my_abs(-6))
